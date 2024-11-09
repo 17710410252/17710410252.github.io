590c92b51d90da623ed2290d755d58a995315f35
17710410252 <Karen8868@126.com>
2024-11-09 15:24:49 +0800

    Update _make_favicon.sh

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,1 +1,1 @@
-convert -background transparent "王宇宸证件照正方形.jpg" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+convert -background transparent "YWphoto.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
